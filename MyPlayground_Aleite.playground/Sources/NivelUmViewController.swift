a2ceca6d2f321d20f836fbc245306f76275fcf00
Meyrillan Souza da Silva <meyri@br-ufpe-11.local>
2020-04-09 22:15:15 -0300

    Primeiras jogadas

--- MyPlayground_Aleite.playground/Sources/NivelUmViewController.swift
+++ MyPlayground_Aleite.playground/Sources/NivelUmViewController.swift
@@ -8,13 +8,21 @@ public class NivelUmViewController : UIViewController {
     let viewSprite = SKView(frame: CGRect(x: 0, y: 0, width: 1440, height: 900))
     let scene = SKScene(size: CGSize(width: 1440, height: 900))
     
-    var admDireita = SKSpriteNode()
-    var andandoFrames: [SKTexture] = []
+    var adm = SKSpriteNode()
+    var andandoFramesParaCima: [SKTexture] = []
+    var andandoFramesParaBaixo: [SKTexture] = []
+    
+    var entregador = SKSpriteNode()
+    var entregadorAndandoParaCima: [SKTexture] = []
+    var entregadorAndandoParaBaixo: [SKTexture] = []
+    
+    var mudandoPontuacao: [UIImage] = []
     
     let cenario = SKSpriteNode(imageNamed: "Cenário@2x.png")
     let armario = SKSpriteNode(imageNamed: "Armário@2x.png")
     let balcao = SKSpriteNode(imageNamed: "Balcão@2x.png")
     let freezer = SKSpriteNode(imageNamed: "FreezerSemLeite@2x.png")
+    let viewFreezer = UIImageView()
     let lavaLouca = SKSpriteNode(imageNamed: "LavaLouca@2x.png")
     let filtroAgua = SKSpriteNode(imageNamed: "filtroAgua@2x.png")
     let sofa = SKSpriteNode(imageNamed: "sofa@2x.png")
@@ -33,7 +41,7 @@ public class NivelUmViewController : UIViewController {
     let viewBgPontuacao = UIImageView()
     let imageBgViewPontuacao = UIImage(named: "ViewPontuacao@2x.png")
     let viewPontuacao = UIImageView()
-    let imagePontuacaoVazia = UIImage(named: "PontuacaoVazia@2x.png")
+    var imagePontuacao = UIImage(named: "Pontuacao0@2x.png")
     let labelPontuacao = UILabel()
     let viewBgReacoes = UIImageView()
     let imageBgViewReacoes = UIImage(named: "viewReacoes@2x.png")
@@ -72,42 +80,46 @@ public class NivelUmViewController : UIViewController {
         viewMissoes.frame = CGRect(x: 0, y: 0, width: 314, height: 333)
         viewMissoes.image = imageViewMissoes
         
-        labelMissoes.frame = CGRect(x: 36, y: 36, width: 68, height: 21)
+        labelMissoes.frame = CGRect(x: 30, y: 36, width: 68, height: 21)
         labelMissoes.text = "Missões"
         labelMissoes.font = sfBoldMaior
         labelMissoes.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
-        labelMissaoUm.frame = CGRect(x: 36, y: 73, width: 250, height: 16)
+        labelMissaoUm.frame = CGRect(x: 30, y: 73, width: 250, height: 16)
         labelMissaoUm.text = "Doe 1 frasco de leite para um hospital"
-        labelMissaoUm.font = sfRegular
+        labelMissaoUm.font = sfBoldMenor
         labelMissaoUm.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
-        buttonDoar.frame = CGRect(x: 62, y: 105, width: 165, height: 56)
+        buttonDoar.frame = CGRect(x: 58, y: 105, width: 165, height: 56)
         buttonDoar.setTitle("Doar", for: .normal)
         buttonDoar.titleLabel?.font = sfBoldMenor
         buttonDoar.backgroundColor = #colorLiteral(red: 1, green: 0.4862745098, blue: 0.5294117647, alpha: 1)
         buttonDoar.layer.cornerRadius = 14
         
-        labelMissaoDois.frame = CGRect(x: 36, y: 185, width: 250, height: 16)
+        labelMissaoDois.frame = CGRect(x: 30, y: 185, width: 270, height: 40)
         labelMissaoDois.text = "Faça o entregador deixar o leite no freezer"
         labelMissaoDois.font = sfRegular
         labelMissaoDois.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        labelMissaoDois.alpha = 0.5
         
-        labelMissaoTres.frame = CGRect(x: 36, y: 225, width: 250, height: 40)
+        labelMissaoTres.frame = CGRect(x: 30, y: 225, width: 270, height: 40)
         labelMissaoTres.text = "Faça o administrador entregar o leite para \na mãe"
         labelMissaoTres.numberOfLines = 2
         labelMissaoTres.font = sfRegular
         labelMissaoTres.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        labelMissaoTres.alpha = 0.5
         
-        labelMissaoQuatro.frame = CGRect(x: 36, y: 281, width: 190, height: 16)
+        labelMissaoQuatro.frame = CGRect(x: 30, y: 281, width: 190, height: 16)
         labelMissaoQuatro.text = "Faça a mãe alimentar o bebê"
         labelMissaoQuatro.font = sfRegular
         labelMissaoQuatro.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        labelMissaoQuatro.alpha = 0.5
         
         viewBgPontuacao.frame = CGRect(x: 366, y: 0, width: 144, height: 217)
         viewBgPontuacao.image = imageBgViewPontuacao
+        
         viewPontuacao.frame = CGRect(x: 403, y: 79.18, width: 70.96, height: 105.82)
-        viewPontuacao.image = imagePontuacaoVazia
+        viewPontuacao.image = imagePontuacao
         
         labelPontuacao.frame = CGRect(x: 392, y: 36, width: 92, height: 21)
         labelPontuacao.text = "Pontuação"
@@ -151,7 +163,16 @@ public class NivelUmViewController : UIViewController {
         labelBebeFeliz.font = sfRegularMaior
         labelBebeFeliz.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
+        viewFreezer.frame = CGRect(x: 636, y: 217, width: 90, height: 144.2)
+        viewFreezer.image = UIImage(named: "FreezerTransparente@2x.png")
+        viewFreezer.isUserInteractionEnabled = false
+        let tapFreezer = UITapGestureRecognizer(target: self, action: #selector(handleTapInFreezer))
+        viewFreezer.addGestureRecognizer(tapFreezer)
+        
+        pontuacao()
+        
         view.addSubview(viewSprite)
+        view.addSubview(viewFreezer)
         view.addSubview(buttonInicio)
         view.addSubview(viewMissoes)
         view.addSubview(labelMissoes)
@@ -190,29 +211,74 @@ public class NivelUmViewController : UIViewController {
         cenario.position = CGPoint(x: 0, y: 0)
         // GestureRecognizer
         cenario.isUserInteractionEnabled = true
-        let tapCenario = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        let tapCenario = UITapGestureRecognizer(target: self, action: #selector(handleTapInPiso))
         viewSprite.addGestureRecognizer(tapCenario)
         
+        lavaLouca.physicsBody = SKPhysicsBody(rectangleOf: lavaLouca.size)
+        lavaLouca.physicsBody?.affectedByGravity = false
+        lavaLouca.physicsBody?.allowsRotation = false
+        lavaLouca.physicsBody?.isDynamic = false
+
         lavaLouca.setScale(1)
         lavaLouca.position = scene.convertPoint(fromView: CGPoint(x: 787, y: 250))
+        
+        freezer.physicsBody = SKPhysicsBody(rectangleOf: freezer.size)
+        freezer.physicsBody?.affectedByGravity = false
+        freezer.physicsBody?.allowsRotation = false
+        freezer.physicsBody?.isDynamic = false
         
         freezer.setScale(1)
         freezer.position = scene.convertPoint(fromView: CGPoint(x: 680, y: 291))
         
+        balcao.physicsBody = SKPhysicsBody(rectangleOf: balcao.size)
+        balcao.physicsBody?.affectedByGravity = false
+        balcao.physicsBody?.allowsRotation = false
+        balcao.physicsBody?.isDynamic = false
+        
         balcao.setScale(1)
         balcao.position = scene.convertPoint(fromView: CGPoint(x: 565, y: 422))
+        
+        armario.physicsBody = SKPhysicsBody(rectangleOf: armario.size)
+        armario.physicsBody?.affectedByGravity = false
+        armario.physicsBody?.allowsRotation = false
+        armario.physicsBody?.isDynamic = false
         
         armario.setScale(1)
         armario.position = scene.convertPoint(fromView: CGPoint(x: 389, y: 457))
         
+        filtroAgua.physicsBody = SKPhysicsBody(rectangleOf: filtroAgua.size)
+        filtroAgua.physicsBody?.affectedByGravity = false
+        filtroAgua.physicsBody?.allowsRotation = false
+        filtroAgua.physicsBody?.isDynamic = false
+        
         filtroAgua.setScale(1)
         filtroAgua.position = scene.convertPoint(fromView: CGPoint(x: 894, y: 256))
+        
+        sofa.physicsBody = SKPhysicsBody(rectangleOf: sofa.size)
+        sofa.physicsBody?.affectedByGravity = false
+        sofa.physicsBody?.allowsRotation = false
+        sofa.physicsBody?.isDynamic = false
         
         sofa.setScale(1)
         sofa.position = scene.convertPoint(fromView: CGPoint(x: 1040, y: 373))
         
+        centroSofa.physicsBody = SKPhysicsBody(rectangleOf: centroSofa.size)
+        centroSofa.physicsBody?.affectedByGravity = false
+        centroSofa.physicsBody?.allowsRotation = false
+        centroSofa.physicsBody?.isDynamic = false
+        
         centroSofa.setScale(1)
         centroSofa.position = scene.convertPoint(fromView: CGPoint(x: 1247, y: 496))
+        
+        adm.physicsBody = SKPhysicsBody(rectangleOf: adm.size)
+        adm.physicsBody?.affectedByGravity = false
+        adm.physicsBody?.allowsRotation = false
+        adm.physicsBody?.isDynamic = false
+        
+        buildAdmParaCima()
+        buildAdmParaBaixo()
+        buildEntregadorParaCima()
+        buildEntregadorParaBaixo()
         
         scene.addChild(cenario)
         scene.addChild(lavaLouca)
@@ -222,30 +288,89 @@ public class NivelUmViewController : UIViewController {
         scene.addChild(filtroAgua)
         scene.addChild(sofa)
         scene.addChild(centroSofa)
-        buildAdmDireita()
+        scene.addChild(adm)
+        scene.addChild(entregador)
         
     }
-    
-//    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        let touch = touches.first!
-//        let location = touch.location(in: viewSprite)
-//        moveAdm(location: location)
-//    }
     
     @IBAction public func touchedButtonInicio() {
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction public func touchedButtonDoar() {
+        viewPontuacao.image = mudandoPontuacao[1]
+        
+        buttonDoar.isEnabled = false
+        buttonDoar.alpha = 0.5
+        
+        labelMissaoUm.alpha = 0.5
+        labelMissaoUm.font = sfRegular
+        
+        labelMissaoDois.alpha = 1
+        labelMissaoDois.font = sfBoldMenor
+        
+        moveEntregador(location: scene.convertPoint(fromView: CGPoint(x: 560, y: 605)))
+        
+        viewFreezer.isUserInteractionEnabled = true
         
     }
     
-    @objc func handleTap(_ gesture: UIGestureRecognizer) {
-        //        animateAdmDireita()
-        moveAdm(location: scene.convertPoint(fromView: gesture.location(in: viewSprite)))
+    @objc func handleTapInFreezer(_ gesture: UIGestureRecognizer) {
+        
+        moveEntregador(location: scene.convertPoint(fromView: CGPoint(x: 713, y: 320)))
+        
+        viewPontuacao.image = mudandoPontuacao[2]
+        
+        labelMissaoDois.alpha = 0.5
+        labelMissaoDois.font = sfRegular
+        
+        labelMissaoTres.alpha = 1
+        labelMissaoTres.font = sfBoldMenor
+
     }
     
-    func buildAdmDireita() {
+    @objc func handleTapInPiso(_ gesture: UIGestureRecognizer) {
+//        moveAdm(location: scene.convertPoint(fromView: gesture.location(in: viewSprite)))
+//        moveEntregador(location: scene.convertPoint(fromView: gesture.location(in: viewSprite)))
+    }
+    
+    func pontuacao() {
+
+        let comecoNome = "Pontuacao"
+        let índices = [0,1,2,3,4,5]
+        
+        for i in índices {
+            let umFramePontuacao = UIImage(named: "\(comecoNome)\(i)@2x.png")
+            mudandoPontuacao.append(umFramePontuacao!)
+        }
+        
+//        viewPontuacao.animationImages = imagensAnimação
+//        viewPontuacao.animationDuration = 0.5
+//        viewPontuacao.startAnimating()
+        
+    }
+    
+    func buildAdmParaCima() {
+        
+        let comecoNome = "ADM - Esquerda"
+        let índices = [0,1,2,3]
+        var imagensAnimação: [SKTexture] = []
+        
+        for i in índices {
+            let umaPoseAdmEsquerda = SKTexture(imageNamed: "\(comecoNome)\(i)")
+            imagensAnimação.append(umaPoseAdmEsquerda)
+        }
+        
+        andandoFramesParaCima = imagensAnimação
+        
+        let firstFrameTexture = andandoFramesParaCima[0]
+        adm = SKSpriteNode(texture: firstFrameTexture)
+        adm.setScale(1.2)
+        adm.position = scene.convertPoint(fromView: CGPoint(x: 600, y: 450))
+        
+    }
+    
+    func buildAdmParaBaixo() {
         //        let admDireitaAnimatedAtlas = SKTextureAtlas(named: "ADMDireita")
         //        var animacaoAdmDireita: [SKTexture] = []
         //
@@ -265,19 +390,22 @@ public class NivelUmViewController : UIViewController {
         }
         
         //        andandoFrames = animacaoAdmDireita
-        andandoFrames = imagensAnimação
+        andandoFramesParaBaixo = imagensAnimação
         
-        let firstFrameTexture = andandoFrames[0]
-        admDireita = SKSpriteNode(texture: firstFrameTexture)
-        admDireita.setScale(1.2)
-        admDireita.position = scene.convertPoint(fromView: CGPoint(x: 700, y: 300))
-        scene.addChild(admDireita)
+        let firstFrameTexture = andandoFramesParaBaixo[0]
+        adm = SKSpriteNode(texture: firstFrameTexture)
+        adm.setScale(1.2)
+        adm.position = scene.convertPoint(fromView: CGPoint(x: 600, y: 450))
         
     }
     
-    func animateAdmDireita() {
+    func animateAdmParaCima() {
+        adm.run(SKAction.repeatForever(SKAction.animate(with: andandoFramesParaCima, timePerFrame: 0.2, resize: false, restore: true)), withKey:"andandoParaCima")
+    }
+    
+    func animateAdmParaBaixo() {
         
-        admDireita.run(SKAction.repeatForever(SKAction.animate(with: andandoFrames, timePerFrame: 0.3, resize: false, restore: true)), withKey:"andandoParaDireita")
+        adm.run(SKAction.repeatForever(SKAction.animate(with: andandoFramesParaBaixo, timePerFrame: 0.2, resize: false, restore: true)), withKey:"andandoParaBaixo")
         //        let animação = SKAction.animate(with: andandoFrames, timePerFrame: 3.0/20.0)
         //        let animaçãoRepetida = SKAction.repeatForever(animação)
         //        admDireita.run(animaçãoRepetida)
@@ -285,24 +413,31 @@ public class NivelUmViewController : UIViewController {
     }
     
     func moveAdm(location: CGPoint) {
-        var multiplierForDirection: CGFloat
-        let admSpeed = viewSprite.frame.size.width / 9.0
+        var multiplierForDirection: CGFloat = 1.0
+        let admSpeed = viewSprite.frame.size.width / 22.0
         
-        let moveDifference = CGPoint(x: location.x - admDireita.position.x, y: location.y - admDireita.position.y)
+        let moveDifference = CGPoint(x: location.x - adm.position.x, y: location.y - adm.position.y)
         let distanceToMove = sqrt(moveDifference.x * moveDifference.x + moveDifference.y * moveDifference.y)
         
         let moveDuration = distanceToMove / admSpeed
         
-        if moveDifference.x < 0 {
-            multiplierForDirection = 1.0
+        if (location.y > adm.position.y) {
+            
+            if adm.action(forKey: "andandoParaCima") == nil {
+                // Se moveDifference for maior que 0, multiplierForDirection = -1.0 senão, é igual a 1.0
+                multiplierForDirection = moveDifference.x > 0 ? -1.0 : 1.0
+                animateAdmParaCima()
+            }
+            
         } else {
-            multiplierForDirection = -1.0
+            
+            if adm.action(forKey: "andandoParaBaixo") == nil {
+                multiplierForDirection = moveDifference.x < 0 ? -1.0 : 1.0
+                animateAdmParaBaixo()
+            }
         }
-        admDireita.xScale = abs(admDireita.xScale) * multiplierForDirection
         
-        if admDireita.action(forKey: "andandoParaDireita") == nil {
-            animateAdmDireita()
-        }
+        adm.xScale = abs(adm.xScale) * multiplierForDirection
         
         let moveAction = SKAction.move(to: location, duration:(TimeInterval(moveDuration)))
         
@@ -311,11 +446,101 @@ public class NivelUmViewController : UIViewController {
         })
         
         let moveActionWithDone = SKAction.sequence([moveAction, doneAction])
-        admDireita.run(moveActionWithDone, withKey:"admMoving")
+        adm.run(moveActionWithDone, withKey:"admMoving")
+        
     }
     
     func admMoveEnded() {
-        admDireita.removeAllActions()
+        adm.removeAllActions()
+    }
+    
+    func buildEntregadorParaCima() {
+        
+        let comecoNome = "ADM - Esquerda"
+        let índices = [0,1,2,3]
+        var imagensAnimação: [SKTexture] = []
+        
+        for i in índices {
+            let umaPoseEntregadorEsquerda = SKTexture(imageNamed: "\(comecoNome)\(i)")
+            imagensAnimação.append(umaPoseEntregadorEsquerda)
+        }
+        
+        entregadorAndandoParaCima = imagensAnimação
+        
+        entregador.setScale(1.2)
+        entregador.position = scene.convertPoint(fromView: CGPoint(x: -10, y: 950))
+        
+    }
+    
+    func buildEntregadorParaBaixo() {
+        
+        let comecoNome = "ADM  - Direita"
+        let índices = [0,1,2,3]
+        var imagensAnimação: [SKTexture] = []
+        
+        for i in índices {
+            let umaPoseEntregadorDireita = SKTexture(imageNamed: "\(comecoNome)\(i)")
+            imagensAnimação.append(umaPoseEntregadorDireita)
+        }
+        
+        entregadorAndandoParaBaixo = imagensAnimação
+        
+        let firstFrameTexture = entregadorAndandoParaBaixo[0]
+        entregador = SKSpriteNode(texture: firstFrameTexture)
+        entregador.setScale(1.2)
+        entregador.position = scene.convertPoint(fromView: CGPoint(x: -10, y: 950))
+        
+    }
+    
+    func animateEntregadorParaCima() {
+         entregador.run(SKAction.repeatForever(SKAction.animate(with: entregadorAndandoParaCima, timePerFrame: 0.2, resize: false, restore: true)), withKey:"entregadorAndandoParaCima")
+     }
+     
+     func animateEntregadorParaBaixo() {
+         
+         entregador.run(SKAction.repeatForever(SKAction.animate(with: entregadorAndandoParaBaixo, timePerFrame: 0.2, resize: false, restore: true)), withKey:"entregadorAndandoParaBaixo")
+     }
+    
+    func moveEntregador(location: CGPoint) {
+        var multiplierForDirection: CGFloat = 1.0
+        let entregadorSpeed = viewSprite.frame.size.width / 22.0
+        
+        let moveDifference = CGPoint(x: location.x - entregador.position.x, y: location.y - entregador.position.y)
+        let distanceToMove = sqrt(moveDifference.x * moveDifference.x + moveDifference.y * moveDifference.y)
+        
+        let moveDuration = distanceToMove / entregadorSpeed
+        
+        if (location.y > entregador.position.y) {
+            
+            if entregador.action(forKey: "entregadorAndandoParaCima") == nil {
+                // Se moveDifference for maior que 0, multiplierForDirection = -1.0 senão, é igual a 1.0
+                multiplierForDirection = moveDifference.x > 0 ? -1.0 : 1.0
+                animateEntregadorParaCima()
+            }
+            
+        } else {
+            
+            if adm.action(forKey: "entregadorAndandoParaBaixo") == nil {
+                multiplierForDirection = moveDifference.x < 0 ? -1.0 : 1.0
+                animateEntregadorParaBaixo()
+            }
+        }
+        
+        entregador.xScale = abs(entregador.xScale) * multiplierForDirection
+        
+        let moveAction = SKAction.move(to: location, duration:(TimeInterval(moveDuration)))
+        
+        let doneAction = SKAction.run({ [weak self] in
+            self?.entregadorMoveEnded()
+        })
+        
+        let moveActionWithDone = SKAction.sequence([moveAction, doneAction])
+        entregador.run(moveActionWithDone, withKey:"entregadorMoving")
+        
+    }
+    
+    func entregadorMoveEnded() {
+        entregador.removeAllActions()
     }
     
 }
